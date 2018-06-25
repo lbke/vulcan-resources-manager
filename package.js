5a953e4f418da16ab3f677672afb4524b4f126f1
Eric Burel <e.burel.contact@gmail.com>
2018-06-25 21:54:18 +0200

    set up package, need to fix the schemas

--- package.js
+++ package.js
@@ -1,24 +1,24 @@
 Package.describe({
-  name: 'vulcan-resources-manager',
-  version: '0.0.1',
+  name: "vulcan-resources-manager",
+  version: "0.0.1",
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: "",
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: "",
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: "README.md"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.7.0.3');
-  api.use('ecmascript');
-  api.mainModule('vulcan-resources-manager.js');
-});
+  api.versionsFrom("1.7.0.3");
+  api.use("ecmascript");
+  // api.mainModule("vulcan-resources-manager.js");
+  api.mainModule("lib/server/main.js", "server");
+  api.mainModule("lib/client/main.js", "client");
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('vulcan-resources-manager');
-  api.mainModule('vulcan-resources-manager-tests.js');
+  api.addAssets(
+    ["lib/assets/schemaorg-vulcanized.jsonld"],
+    ["client", "server"]
+  );
 });
